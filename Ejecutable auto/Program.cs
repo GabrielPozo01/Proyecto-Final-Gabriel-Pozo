6ec72b8b30a11595bd7c5bd8b8a0fc317a8e5cbc
GabrielPozo01 <109257405+GabrielPozo01@users.noreply.github.com>
2024-04-18 01:22:51 +0100

    Add files via upload

---
+++ Ejecutable auto/Program.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Support.UI;
+
+
+
+using Proyecto_final_automatizado;
+
+namespace MyExecutableProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ejecutar las pruebas automatizadas
+            var testClass = new LoginAndRegisterTests();
+            testClass.EdgeDriverInitialize();
+            testClass.Login_With_Valid_Credentials_Should_Succeed();
+            testClass.Register_New_User_Should_Succeed();
+            testClass.EdgeDriverCleanup();
+        }
+    }
+}
+
+
+
+
+
