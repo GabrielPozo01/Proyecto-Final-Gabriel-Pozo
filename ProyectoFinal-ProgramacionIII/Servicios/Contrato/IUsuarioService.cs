570861c8c384345d3b1ecc896dbd51cbbbd79930
GabrielPozo01 <109257405+GabrielPozo01@users.noreply.github.com>
2024-04-17 20:33:21 -0400

    Add files via upload

---
+++ ProyectoFinal-ProgramacionIII/Servicios/Contrato/IUsuarioService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinal_ProgramacionIII;
+using ProyectoFinal_ProgramacionIII.Models;
+
+
+namespace ProyectoFinal_ProgramacionIII.Servicios.Contrato
+{
+    public interface IUsuarioService
+    {
+
+        Task<Usuario> GetUsuario(string correo, string clave);
+
+        Task<Usuario> SaveUsuario(Usuario modelo);
+
+    }
+}
---
+++ ProyectoFinal-ProgramacionIII/Servicios/Implementacion/UsuarioService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProyectoFinal_ProgramacionIII.Models;
+using ProyectoFinal_ProgramacionIII.Servicios.Contrato;
+
+namespace ProyectoFinal_ProgramacionIII.Servicios.Implementacion
+{
+    public class UsuarioService : IUsuarioService
+    {
+
+        private readonly DbfinalContext _dbContext;
+
+        public UsuarioService(DbfinalContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Usuario> GetUsuario(string correo, string clave)
+        {
+            Usuario usuario_encontrado = await _dbContext.Usuarios.Where(u => u.Correo == correo && u.Clave == clave).FirstOrDefaultAsync();
+
+            return usuario_encontrado;
+        }
+
+        public async Task<Usuario> SaveUsuario(Usuario modelo) 
+        {
+            _dbContext.Usuarios.Add(modelo);
+            await _dbContext.SaveChangesAsync();
+            return modelo;
+        }
+
+    }
+}
